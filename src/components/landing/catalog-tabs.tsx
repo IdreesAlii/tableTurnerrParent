e696307d6aa62e4cc97d50848bc215aae431f470
Hashaam Zahid <hashaam.zahid.work@gmail.com>
2025-11-02 19:54:20 +0000

    Try fixing this error: `Runtime Error: Error: Button is not defined. Err

--- src/components/landing/catalog-tabs.tsx
+++ src/components/landing/catalog-tabs.tsx
@@ -6,6 +6,7 @@ import { PlaceHolderImages } from '@/lib/placeholder-images';
 import Image from 'next/image';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
 const tabsData = [
   {
